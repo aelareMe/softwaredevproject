575c4b8e898d55ab83b78eb2e0895a10fdccc96d
jaygwapo <82523620+jaygwapo@users.noreply.github.com>
2021-04-18 15:58:02 +0800

    first commit

---
+++ Project/Project/View/AddEvent.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Project
+{
+    partial class AddEvent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.addEventConfirm = new System.Windows.Forms.Button();
+            this.eventDate = new System.Windows.Forms.DateTimePicker();
+            this.eventName = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(25, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "BACK";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // addEventConfirm
+            // 
+            this.addEventConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addEventConfirm.Location = new System.Drawing.Point(130, 163);
+            this.addEventConfirm.Name = "addEventConfirm";
+            this.addEventConfirm.Size = new System.Drawing.Size(116, 44);
+            this.addEventConfirm.TabIndex = 12;
+            this.addEventConfirm.Text = "CONFIRM";
+            this.addEventConfirm.UseVisualStyleBackColor = true;
+            // 
+            // eventDate
+            // 
+            this.eventDate.Location = new System.Drawing.Point(141, 117);
+            this.eventDate.Name = "eventDate";
+            this.eventDate.Size = new System.Drawing.Size(200, 20);
+            this.eventDate.TabIndex = 11;
+            // 
+            // eventName
+            // 
+            this.eventName.Location = new System.Drawing.Point(141, 70);
+            this.eventName.Name = "eventName";
+            this.eventName.Size = new System.Drawing.Size(203, 17);
+            this.eventName.TabIndex = 10;
+            this.eventName.Text = "";
+            
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "DATE:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "EVENT NAME:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(126, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ADD EVENT";
+            // 
+            // AddEvent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 296);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.addEventConfirm);
+            this.Controls.Add(this.eventDate);
+            this.Controls.Add(this.eventName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddEvent";
+            this.Text = "AddEvent";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button addEventConfirm;
+        private System.Windows.Forms.DateTimePicker eventDate;
+        private System.Windows.Forms.RichTextBox eventName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Project/Project/View/AddEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class AddEvent : Form
+    {
+        public Event e1;
+        public bool confirm_flag;
+        public AddEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e) //exit button
+        {
+            AddEvent.ActiveForm.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //add event button
+        {
+            if (eventName.Text == "" || (eventDate.Value - DateTime.Today).TotalDays < 0) //if blank ang event name or ang date nga giset kay humana with respect sa date today
+            {
+                MessageBox.Show("Blank event name or event date can't be due past with today. Please try again.");
+                return;
+            }
+            else
+            {
+                e1 = new Event(eventName.Text, eventDate.Value);
+                confirm_flag = true;
+                MessageBox.Show("Success");
+                return;
+            }
+
+
+        }
+    }
+}
