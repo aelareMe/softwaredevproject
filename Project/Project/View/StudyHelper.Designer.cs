8e1deac95978108a5df444107f180264a716f6b0
jaygwapo <82523620+jaygwapo@users.noreply.github.com>
2021-04-18 16:32:30 +0800

    first commit

---
+++ Project/Project/View/StudyHelper.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Project
+{
+    partial class StudyHelper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.percentLbl = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.messageBox = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.setBtn = new System.Windows.Forms.Button();
+            this.percentTb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // percentLbl
+            // 
+            this.percentLbl.AutoSize = true;
+            this.percentLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.percentLbl.Location = new System.Drawing.Point(235, 206);
+            this.percentLbl.Name = "percentLbl";
+            this.percentLbl.Size = new System.Drawing.Size(16, 13);
+            this.percentLbl.TabIndex = 25;
+            this.percentLbl.Text = "%";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(13, 315);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 23);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "ACTIVITY SCHEDULER";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(387, 315);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 23);
+            this.exitBtn.TabIndex = 23;
+            this.exitBtn.Text = "EXIT";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(203, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 15);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "MESSAGE:";
+            // 
+            // messageBox
+            // 
+            this.messageBox.Location = new System.Drawing.Point(48, 254);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(372, 55);
+            this.messageBox.TabIndex = 21;
+            this.messageBox.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(177, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 16);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "YOUR PROGRESS:";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(48, 180);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(372, 23);
+            this.progressBar.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "STUDY PROGRESS (%):";
+            // 
+            // setBtn
+            // 
+            this.setBtn.Location = new System.Drawing.Point(274, 95);
+            this.setBtn.Name = "setBtn";
+            this.setBtn.Size = new System.Drawing.Size(75, 32);
+            this.setBtn.TabIndex = 17;
+            this.setBtn.Text = "SET";
+            this.setBtn.UseVisualStyleBackColor = true;
+            // 
+            // percentTb
+            // 
+            this.percentTb.Location = new System.Drawing.Point(165, 102);
+            this.percentTb.Name = "percentTb";
+            this.percentTb.Size = new System.Drawing.Size(86, 20);
+            this.percentTb.TabIndex = 16;
+            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 32);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "STUDY HELPER";
+            // 
+            // StudyHelper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 357);
+            this.Controls.Add(this.percentLbl);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.messageBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.setBtn);
+            this.Controls.Add(this.percentTb);
+            this.Controls.Add(this.label1);
+            this.Name = "StudyHelper";
+            this.Text = "StudyHelper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label percentLbl;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox messageBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button setBtn;
+        private System.Windows.Forms.TextBox percentTb;
+        private System.Windows.Forms.Label label1;
+    }
+}
